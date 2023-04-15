2415e7510e76a3be9596da6f791f3a324759f7eb
shaikareef081 <shaikareef482@gmail.com>
2023-04-15 13:14:58 +0530

    second push

--- frontend/src/component/SingleChat.js
+++ frontend/src/component/SingleChat.js
@@ -22,7 +22,7 @@ import ScrollableChat from "./ScrollableChat";
 
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chatapp-web.onrender.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
