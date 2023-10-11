2d5882ee79ebae1943ef5f38f9c9fbf0153c07b1
shaikareef081 <shaikareef482@gmail.com>
2023-10-11 20:03:34 +0530

    chnaged the root folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -8,16 +8,28 @@ import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <ChakraProvider>
+//         <ChatProvider>
+//           <App />
+//         </ChatProvider>
+//       </ChakraProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+
+//   //  document.getElementById("root")
+// );
+
+ReactDOM.render(
+  <BrowserRouter>
       <ChakraProvider>
         <ChatProvider>
           <App />
         </ChatProvider>
       </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-
-  //  document.getElementById("root")
+    </BrowserRouter>,
+     document.getElementById("root")
 );
+
